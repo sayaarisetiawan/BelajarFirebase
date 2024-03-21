902f96f742a62d5d884e78ed9d1728e8b4a13d65
Ari Setiawan <ari_22510010@stimata.ac.id>
2024-03-21 23:52:38 +0700

    Tugas Pertemuan 2
    Praktikum PPB 2 realtime database

--- app/src/main/java/com/example/belajarfirebase/MainActivity.kt
+++ app/src/main/java/com/example/belajarfirebase/MainActivity.kt
@@ -11,6 +11,8 @@ import com.firebase.ui.auth.AuthUI
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var auth: FirebaseAuth? = null
@@ -34,6 +36,38 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (p0?.getId()) {
             R.id.save -> {
 // Statement program untuk simpan data
+                val getUserID = auth!!.currentUser!!.uid
+//Mendapatkan Instance dari Database
+
+                val database = FirebaseDatabase.getInstance()
+//Menyimpan Data yang diinputkan User kedalam Variable
+                val getNIM: String = binding.nim.getText().toString()
+                val getNama: String = binding.nama.getText().toString()
+                val getJurusan: String = binding.jurusan.getText().toString()
+// Mendapatkan Referensi dari Database
+                val getReference: DatabaseReference
+                getReference = database.reference
+// Mengecek apakah ada data yang kosong
+                if (isEmpty(getNIM) || isEmpty(getNama) || isEmpty(getJurusan)) {
+//Jika Ada, maka akan menampilkan pesan singkan seperti berikut ini.
+                    Toast.makeText(this@MainActivity, "Data tidak boleh ada yang kosong",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    /* Jika Tidak, maka data dapat diproses dan meyimpannya pada Database
+                    Menyimpan data referensi pada Database berdasarkan User ID dari masing-masing
+                    Akun
+                    */
+                    getReference.child("Admin").child(getUserID).child("Mahasiswa").push()
+                        .setValue(data_mahasiswa(getNIM, getNama, getJurusan))
+                        .addOnCompleteListener(this) {
+                            //Peristiwa ini terjadi saat user berhasil menyimpan datanya kedalam Database
+                            binding.nim.setText("")
+                            binding.nama.setText("")
+                            binding.jurusan.setText("")
+                            Toast.makeText(this@MainActivity, "Data Tersimpan",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                }
             }
             R.id.logout ->
 // Statement program untuk logout/keluar
